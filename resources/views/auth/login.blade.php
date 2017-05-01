487551f57f5ad606bca3a685604bf3ee064e08be
anisku11 <aan.faisalal@gmail.com>
2017-05-01 21:45:55 +0700

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,9 +6,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
    <title>Sistem Informasi Audit Energi Listrik | UNWAHAS </title>
-    
+
     <!-- Bootstrap -->
     <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet">
     <!-- Font Awesome -->
@@ -27,7 +27,7 @@
             <section class="login_content">
                 <form method="post" action="{{ url('/login') }}">
                     {!! csrf_field() !!}
-                    
+
                     <h1>Login ke Sipetri!</h1>
                     <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
                         <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
@@ -38,7 +38,7 @@
                 </span>
                         @endif
                     </div>
-                    
+
                     <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
                         <input type="password" class="form-control" placeholder="Password" name="password">
                         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
@@ -47,26 +47,26 @@
                   <strong>{{ $errors->first('password') }}</strong>
                 </span>
                         @endif
-                    
+
                     </div>
                     <div>
                         <input type="submit" class="btn btn-default submit" value="Log in">
                         <a class="reset_pass" href="{{  url('/password/reset') }}">Lupa password?</a>
                     </div>
-                    
+
                     <div class="clearfix"></div>
-                    
+
                     <div class="separator">
                         <p class="change_link">
                             <a href="{{ url('/register') }}" class="to_register"> Buat akun </a>
                         </p>
-                        
+
                         <div class="clearfix"></div>
                         <br />
-                        
+
                         <div>
-                            <h1><i class="fa fa-paw"></i> Sipetri !</h1>
-                            <p>Universitas Wahid Hasyim ©2016</p>
+                            <h1><i class="fa fa-bank"></i> Sipetri !</h1>
+                            <p>Universitas Wahid Hasyim ©2017</p>
                         </div>
                     </div>
                 </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,9 +6,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
    <title>Sistem Informasi Audit Energi Listrik | UNWAHAS </title>
-    
+
     <!-- Bootstrap -->
     <link href="{{ asset("css/bootstrap.min.css") }}" rel="stylesheet">
     <!-- Font Awesome -->
@@ -26,46 +26,46 @@
         <section class="login_content">
             <form method="post" action="{{ url('/register') }}">
                 {!! csrf_field() !!}
-                
+
                 <h1>Buat Akun</h1>
-                
+
                 <div class="form-group has-feedback{{ $errors->has('name') ? ' has-error' : '' }}">
                     <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Full Name">
                     <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                    
+
                     @if ($errors->has('name'))
                         <span class="help-block">
                   <strong>{{ $errors->first('name') }}</strong>
                 </span>
                     @endif
                 </div>
-                
+
                 <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
                     <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                    
+
                     @if ($errors->has('email'))
                         <span class="help-block">
                   <strong>{{ $errors->first('email') }}</strong>
                 </span>
                     @endif
                 </div>
-                
+
                 <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
                     <input type="password" class="form-control" name="password" placeholder="Password">
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    
+
                     @if ($errors->has('password'))
                         <span class="help-block">
                   <strong>{{ $errors->first('password') }}</strong>
                 </span>
                     @endif
                 </div>
-                
+
                 <div class="form-group has-feedback{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                     <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm password">
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                    
+
                     @if ($errors->has('password_confirmation'))
                         <span class="help-block">
                   <strong>{{ $errors->first('password_confirmation') }}</strong>
@@ -75,20 +75,20 @@
                 <div>
                     <button class="btn btn-default submit" >Registrasi</button>
                 </div>
-                
+
                 <div class="clearfix"></div>
-                
+
                 <div class="separator">
                     <p class="change_link">Sudah daftar?
                         <a href="{{ url('/login') }}" class="to_register"> Log in </a>
                     </p>
-                    
+
                     <div class="clearfix"></div>
                     <br />
-                    
+
                         <div>
-                            <h1><i class="fa fa-paw"></i> Sipetri !</h1>
-                            <p>Universitas Wahid Hasyim ©2016</p>
+                            <h1><i class="fa fa-bank"></i> Sipetri !</h1>
+                            <p>Universitas Wahid Hasyim ©2017</p>
                         </div>
                 </div>
             </form>
